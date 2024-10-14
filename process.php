9510323ea5f6b9e719e0f5318229070c0de73e5c
mgarcia14115 <mgarci14115@gmail.com>
2024-10-14 17:28:38 -0500

    adding current webpages

---
+++ process.php
@@ -0,0 +1,10 @@
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        $sepal_length = $_POST["sl"];
+        $sepal_width = $_POST["sw"];
+        $petal_length = $_POST["pl"];
+        $petal_width = $_POST["pw"];
+    }
+    
+?>
